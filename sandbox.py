9ded13d4ca9735169e115a86cfff7a4946996e43
SkanderHmad <skander.hmad@esprit.tn>
2020-07-23 04:22:03 -0700

    Lookup aggregation between repository collection and commits collection

--- sandbox.py
+++ sandbox.py
@@ -1,6 +1,8 @@
 import falcon
 import json
 import pymongo
+from bson import ObjectId
+
 
 class Resource(object):
     def __init__(self):
@@ -21,24 +23,40 @@ class Resource(object):
 
         commits = doc['push']['changes'][0]['commits'][0]
 
+        original_id = ObjectId()
+
         if not self.db.repository.find_one({'full_name': repository['full_name']}):
             self.db.repository.insert_one({
+                '_id': original_id,
                 'provider': 'bitbucket',
                 'key': repository['full_name'],
                 'url': repository['links']['self']['href'],
                 'private': repository['is_private'],
             })
 
-            print('[%s]' % ', '.join(map(str, repository)))
+            #print('[%s]' % ', '.join(map(str, repository)))
 
             self.db.commits.insert({
+                'repository_id': original_id,
                 'hash': commits['hash'],
                 'date': commits['date'],
                 'message': commits['message'],
                 'author': commits['author']['raw'],
 
             })
-            print('[%s]' % ', '.join(map(str, commits)))
+            #print('[%s]' % ', '.join(map(str, commits)))
+
+            self.db.commits.aggregate([
+                {
+                    "$lookup":
+                        {
+                            "from": "repository",
+                            "localField": "repository_id",
+                            "foreignField": "_id",
+                            "as": "test"
+                        }
+                }
+            ])
 
 
         resp.status = falcon.HTTP_200  # This is the default status
