821d37a6bcaaaece650589b5ee8f0ed073d4f01e
SkanderHmad <skander.hmad@esprit.tn>
2020-07-27 14:52:01 -0700

    diff between two commits using git commands

--- sandbox.py
+++ sandbox.py
@@ -1,10 +1,10 @@
-import falcon
 import json
-import git
+import falcon
 import pymongo
 from bson import ObjectId
 from pymongo import MongoClient
-import gitpy
+import git
+import os
 
 class Resource(object):
     def __init__(self):
@@ -12,18 +12,43 @@ class Resource(object):
         self.db = self.mdb.gitsandbox
 
 
+        my_repo = git.Repo('/home/skander/workspace/git-sandbox')
+        #print(my_repo)
+        #diff = my_repo.git.diff(my_repo.head.commit.tree)
+        commits_list = list(my_repo.iter_commits())
+        print("First commit: ", commits_list[0])
+
+        changed_files = []
+
+        for x in commits_list[0].diff(commits_list[-1]):
+            if x.a_blob.path not in changed_files:
+                changed_files.append(x.a_blob.path)
+
+            if x.b_blob is not None and x.b_blob.path not in changed_files:
+                changed_files.append(x.b_blob.path)
+
+        print(changed_files)
+
+
+        #cursor = self.db.commits
+        #hash_list = []
+        #for doc in cursor.find():
+            #hash_list += [doc["hash"]]
+            #print("hash_list:", hash_list)
+
     def on_post(self, req, resp):
         if req.content_length:
             doc = json.load(req.stream)
         if 'push' not in doc or 'actor' not in doc or 'repository' not in doc :
             raise falcon.HTTPBadRequest("Invalid payload received: %s" % doc)
-    
+
         actor = doc['actor']
         repository = doc['repository']
         changes = doc['push']['changes']
         commits = doc['push']['changes'][0]['commits'][0]
 
         original_id = ObjectId()
+
 
         repo_git = self.db.repository.find_one({'key': repository['full_name']})
 
@@ -49,6 +74,8 @@ class Resource(object):
                 'type': commits['type'],
 
             })
+
+
 
         client = MongoClient(
             'mongodb://localhost:27017/?readPreference=primary&appname=MongoDB%20Compass&ssl=false')
