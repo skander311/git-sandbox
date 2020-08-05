f3f81cdf98de252bf69efdbac7080c2aca7a77e3
SkanderHmad <skander.hmad@esprit.tn>
2020-08-05 03:13:33 -0700

    Remove useless lines

--- sandbox.py
+++ sandbox.py
@@ -14,30 +14,6 @@ class Resource(object):
         self.mdb = pymongo.MongoClient("mongodb://localhost:27017/gitsandbox")
         self.db = self.mdb.gitsandbox
 
-    '''
-        path = '/home/skander/workspace/git-sandbox'
-        my_repo = git.Repo(path)
-        commits_list = list(my_repo.iter_commits())
-        for i in commits_list:
-            print(i)
-
-        changed_files = []
-        length = len(commits_list)
-        for j in range(length):
-            for x in commits_list[j].diff(commits_list[j-1]):
-                if x.a_blob.path not in changed_files:
-                    changed_files.append(x.a_blob.path)
-                if x.b_blob is not None and x.b_blob.path not in changed_files:
-                    changed_files.append(x.b_blob.path)
-            print(changed_files)
-
-        cursor = self.db.commits
-        hash_list = []
-        for doc in cursor.find():
-            hash_list += [doc["hash"]]
-            print("hash_list:", hash_list)
-        '''
-
     def on_post(self, req, resp):
         global commit, stats
         if req.content_length:
@@ -76,56 +52,6 @@ class Resource(object):
 
             })
 
-        date_format = "%Y-%m-%dT%H:%M:%S%z"
-        empty_tree = "4b825dc642cb6eb9a060e54bf8d69288fbee4904"
-
-        path = '/home/skander/workspace/git-sandbox'
-        repo = git.Repo(path)
-
-        for commit in repo.iter_commits('master'):
-            parent = commit.parents[0] if commit.parents else empty_tree
-            diffs = {
-                diff.a_path: diff for diff in commit.diff(parent)
-            }
-            for objpath, stats in commit.stats.files.items():
-                diff = diffs.get(objpath)
-
-                if not diff:
-                    for diff in diffs.values():
-                        if diff.b_path == path and diff.renamed:
-                            break
-
-                stats.update({
-                    'object': os.path.join(path, objpath),
-                    'commit': commit.hexsha,
-                    'summary': commit.summary,
-                    'author': commit.author.name,
-                    'author-email': commit.author.email,
-                    'timestamp': commit.authored_datetime,
-                    'type': diff.change_type,
-                    'size': commit.size,
-                })
-
-                print('#####')
-                print(stats)
-                print('#####')
-
-            # if not self.db.commits.find_one({stats['commit']: commits['hash']}):
-            self.db.files.insert({
-                'file name': stats['object'],
-                'hash': stats['commit'],
-                'commit message': stats['summary'],
-                'author': stats['author'],
-                'author email': stats['author-email'],
-                'date': stats['timestamp'],
-                'insertions': stats['insertions'],
-                'deletions': stats['deletions'],
-                'lines': stats['lines'],
-                'type': stats['type'],
-                'size': stats['size'],
-
-            })
-
         client = MongoClient(
             'mongodb://localhost:27017/?readPreference=primary&appname=MongoDB%20Compass&ssl=false')
 
@@ -141,6 +67,82 @@ class Resource(object):
         ])
 
         print(result)
+
+        '''
+                path = '/home/skander/workspace/git-sandbox'
+                my_repo = git.Repo(path)
+                commits_list = list(my_repo.iter_commits())
+                for i in commits_list:
+                    print(i)
+
+                changed_files = []
+                length = len(commits_list)
+                for j in range(length):
+                    for x in commits_list[j].diff(commits_list[j-1]):
+                        if x.a_blob.path not in changed_files:
+                            changed_files.append(x.a_blob.path)
+                        if x.b_blob is not None and x.b_blob.path not in changed_files:
+                            changed_files.append(x.b_blob.path)
+                    print(changed_files)
+
+                cursor = self.db.commits
+                hash_list = []
+                for doc in cursor.find():
+                    hash_list += [doc["hash"]]
+                    print("hash_list:", hash_list)
+                '''
+
+        '''
+                empty_tree = "4b825dc642cb6eb9a060e54bf8d69288fbee4904"
+
+                path = '/home/skander/workspace/git-sandbox'
+                repo = git.Repo(path)
+
+                for commit in repo.iter_commits('master'):
+                    parent = commit.parents[0] if commit.parents else empty_tree
+                    diffs = {
+                        diff.a_path: diff for diff in commit.diff(parent)
+                    }
+                    for objpath, stats in commit.stats.files.items():
+                        diff = diffs.get(objpath)
+
+                        if not diff:
+                            for diff in diffs.values():
+                                if diff.b_path == path and diff.renamed:
+                                    break
+
+                        stats.update({
+                            'object': os.path.join(path, objpath),
+                            'commit': commit.hexsha,
+                            'summary': commit.summary,
+                            'author': commit.author.name,
+                            'author-email': commit.author.email,
+                            'timestamp': commit.authored_datetime,
+                            'type': diff.change_type,
+                            'size': commit.size,
+                        })
+
+                        print('#####')
+                        print(stats)
+                        print('#####')
+
+                    # if not self.db.commits.find_one({stats['commit']: commits['hash']}):
+                    self.db.files.insert({
+                        'file name': stats['object'],
+                        'hash': stats['commit'],
+                        'commit message': stats['summary'],
+                        'author': stats['author'],
+                        'author email': stats['author-email'],
+                        'date': stats['timestamp'],
+                        'insertions': stats['insertions'],
+                        'deletions': stats['deletions'],
+                        'lines': stats['lines'],
+                        'type': stats['type'],
+                        'size': stats['size'],
+
+                    })
+
+        '''
 
         resp.status = falcon.HTTP_200  # This is the default status
         resp.body = ('Okey')
